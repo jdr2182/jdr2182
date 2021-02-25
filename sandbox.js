923697eb607043f3adb95d388f7b136fa458bd2d
jdr2182 <jdr2182@tc.columbia.edu>
2021-02-25 11:28:02 -0500

    ionic

--- sandbox.js
+++
@@ -1,6 +0,0 @@
-import moment from "moment";
-
-var myDate = new Date();
-var myCoolDate = moment(myDate).format("LL");
-
-console.log(myCoolDate);
---
+++ zip-info-pwa/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <ion-app>
+      <ion-vue-router/>
+    </ion-app>
+  </div>
+</template>
+
